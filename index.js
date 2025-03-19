c5b6a2804d41c218434931889688364ebe875f38
Vikas Prasad <76564198+vikas2903@users.noreply.github.com>
2025-03-19 23:25:57 +0530

    Update index.js
    vv

--- index.js
+++ index.js
@@ -27,9 +27,17 @@ if (!fs.existsSync(downloadsDir)) {
 
 // Function to download YouTube audio
 async function downloadAudio(videoUrl, tempFilePath) {
-  const cookiesPath = '/etc/secrets/YOUTUBE_COOKIES';  // Path 
+  
+  // const cookiesPath = '/etc/secrets/YOUTUBE_COOKIES';  // Path 
+  const cookiesPath = '/etc/secrets/YOUTUBE_COOKIES'; 
+  const writableCookiesPath = '/tmp/YOUTUBE_COOKIES';  // Writable path
+  // Copy cookies to a writable location
+  fs.copyFileSync(cookiesPath, writableCookiesPath);
+
   return new Promise((resolve, reject) => {
-    const command = `yt-dlp --cookies "${cookiesPath}" -f bestaudio --output "${tempFilePath}" ${videoUrl}`;
+    // const command = `yt-dlp --cookies "${cookiesPath}" -f bestaudio --output "${tempFilePath}" ${videoUrl}`;
+
+      const command = `yt-dlp --cookies "${writableCookiesPath}" -f bestaudio --output "${tempFilePath}" ${videoUrl}`;
     
     exec(command, (error, stdout, stderr) => {
       if (error) {
